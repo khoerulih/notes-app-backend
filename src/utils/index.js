a623254fd6520832db87ffa1806be06effbd6706
Ikhsan Khoerul <khoerulih.r@gmail.com>
2025-06-03 14:28:59 +0700

    collaboration implemented

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,11 +1,20 @@
 /* eslint-disable camelcase */
-const mapDBToModel = ({ id, title, body, tags, createdAt, updatedAt }) => ({
+const mapDBToModel = ({
+  id,
+  title,
+  body,
+  tags,
+  createdAt,
+  updatedAt,
+  username,
+}) => ({
   id,
   title,
   body,
   tags,
   created_at: createdAt,
   updated_at: updatedAt,
+  username,
 });
 
 module.exports = { mapDBToModel };
